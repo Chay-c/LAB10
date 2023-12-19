4db3866a89f7428267fda01b74bd777be2c32da2
Chay <chayangkul_c@cmu.ac.th>
2023-12-19 15:48:24 +0700

    jaroen

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+        double a,i = 0 ;
+        double sum = 0 ;
+        double sum_of_square ,sd = 0;
+        string textline;
+        ifstream source ;
+        source.open("score.txt");
+        while (getline(source,textline))
+        {
+                a = atof(textline.c_str()) ;
+                sum += a;
+                sd += pow(a,2);
+                i++;
+        }
+        sum = sum/i ;
+        sum_of_square = sqrt((sd/i)-(pow(sum,2))) ;
+        source.close() ;
+        cout << "Number of data = " << i << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << sum << "\n";
+        cout << "Standard deviation = " << sum_of_square ;
+}
